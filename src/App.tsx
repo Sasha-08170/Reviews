1f3d47e637a1c024b2c1e1a3eadf6dbf4cc6a6b9
Sasha-08170 <102165823+Sasha-08170@users.noreply.github.com>
2025-09-12 15:15:22 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ReviewsSlider from './components/ReviewsSlider/ReviewsSlider';
+
+function App() {
+  return (
+    <div>
+      <ReviewsSlider />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ReviewsSlider/ReviewsSlider.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import styles from './ReviewsSlider.module.css';
+import reviews from '../../data/reviews.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+const ReviewsSlider: React.FC = () => {
+  return (
+    <div className={styles['reviews-slider']}>
+      <h2>Reviews Slider</h2> {/* Добавил заголовок, как на фото */}
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={30}
+        slidesPerView={2}
+        navigation={{
+          nextEl: `.${styles['swiper-button-next']}`,
+          prevEl: `.${styles['swiper-button-prev']}`,
+        }}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          320: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+        }}
+      >
+        {reviews.map((review) => (
+          <SwiperSlide key={review.id}>
+            <div className={styles['reviews-slider__card']}>
+              <div className={styles['reviews-slider__header']}>
+                <img
+                  src={review.avatar}
+                  alt={review.name}
+                  className={styles['reviews-slider__avatar']}
+                />
+                <div>
+                  <h3 className={styles['reviews-slider__name']}>{review.name}</h3>
+                  <span className={styles['reviews-slider__source']}>{review.source}</span>
+                </div>
+              </div>
+              <div className={styles['reviews-slider__rating']}>{'★'.repeat(review.rating)}</div>
+              <p className={styles['reviews-slider__text']}>{review.text}</p>
+              <span className={styles['reviews-slider__date']}>{review.date}</span>
+            </div>
+          </SwiperSlide>
+        ))}
+        {/* Кастомные кнопки навигации для Swiper */}
+        <div className={styles['swiper-button-prev']}>
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </div>
+        <div className={styles['swiper-button-next']}>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </div>
+      </Swiper>
+    </div>
+  );
+};
+
+export default ReviewsSlider;
